602cdc5c89f814ad6a21b370735d37f1a076f16d
blaplafla13th <phambathanghp@gmail.com>
2023-01-25 22:53:34 +0700

    fix populate

--- server/src/api/product/routes/product.js
+++ server/src/api/product/routes/product.js
@@ -6,13 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::product.product',{
-  config: {
-    find: {
-      auth: false
-    },
-    findOne: {
-      auth: false
-    },
-  }
-});
+module.exports = createCoreRouter('api::product.product');
