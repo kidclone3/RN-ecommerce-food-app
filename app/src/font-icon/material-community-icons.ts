6caf9e24298bf84d8bf70f7ecb99353dc4acd4eb
kidclone3 <kid.clone.3@gmail.com>
2023-01-27 21:18:33 +0700

    remove old folder

--- app/src/font-icon/material-community-icons.ts
+++
@@ -1,4 +0,0 @@
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-Icon.loadFont();
-const MaterialCommunityIcons = Icon;
-export default MaterialCommunityIcons;
--- app/src/font-icon/material-icons.ts
+++
@@ -1,4 +0,0 @@
-import Icon from 'react-native-vector-icons/MaterialIcons';
-Icon.loadFont().catch((error) => { console.info(error); });
-const MaterialIcons = Icon;
-export default MaterialIcons;
