e9034f0f5ae5a12c3b342693a1f67611ee9d2cb4
lacthanh <92591705+lacthanh@users.noreply.github.com>
2023-02-07 16:44:03 +0700

    categories service

---
+++ MyApp/src/services/categories/GetCategories.js
@@ -0,0 +1,30 @@
+import {API_URL} from '../index';
+import axios from 'axios';
+
+export const getCategories = async (start=0, limit=25) => {
+    let data = [];
+    await
+        axios.get(`${API_URL}/api/categories`, {
+            params: {
+                populate: {
+                    icon: {
+                        fields: "url"
+                    }
+                },
+                pagination: {
+                    start: start,
+                    limit: limit
+                }
+            }
+        })
+            .then(response => {
+                console.warn('Products retrieved');
+                data = response.data.data
+            })
+            .catch(error => {
+                console.warn('Error retrieving products: ' + error);
+                return null;
+            });
+    return data;
+}
+export default getCategories;
