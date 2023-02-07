e2a743f1c7220c9c5556815ca5db2904b0423ffd
lacthanh <92591705+lacthanh@users.noreply.github.com>
2023-02-08 00:24:24 +0700

    add find product

--- MyApp/src/services/products/index.js
+++ MyApp/src/services/products/index.js
@@ -2,4 +2,5 @@ import getComment from "./GetComment";
 import getProducts from "./GetProducts";
 import getProductDetail from "./ProductDetail";
 import getStatsByList from "./GetStatsByList";
-export {getComment,getProducts,getProductDetail,getStatsByList};
+import findProducts from "./FindProducts";
+export {getComment,getProducts,getProductDetail,getStatsByList,findProducts}
