b57a3afa7c549c2f649c83ec91ef8966fb18db18
lacthanh <92591705+lacthanh@users.noreply.github.com>
2023-02-12 23:30:17 +0700

    add hide debug

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -9,46 +9,50 @@
  */
 
 import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {StatusBar, StyleSheet, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 
+// active to hide debug
+// import { LogBox } from 'react-native';
+// LogBox.ignoreAllLogs();
+
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
+    const isDarkMode = useColorScheme() === 'dark';
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        flex: 1,
+    };
 
-  return (
+    return (
 
-    <SafeAreaView style={styles.root} edges={["right", "top", "left"]}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Navigation />
-      {/* <HomeScreen /> */}
-    </SafeAreaView>
-  );
+        <SafeAreaView style={styles.root} edges={["right", "top", "left"]}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
+            <Navigation/>
+            {/* <HomeScreen /> */}
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: '#F9FBFC',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    root: {
+        flex: 1,
+        backgroundColor: '#F9FBFC',
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
 });
 
 export default App;
