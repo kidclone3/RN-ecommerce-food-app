d8ddc6d9517087e44672112fb37b887b584ee083
Lê Thị Thuỳ Dung <lethithuydung_t65@hus.edu.vn>
2023-02-15 02:34:45 +0700

    forgot password screen

---
+++ MyApp/src/hooks/useManagerSendMailResetPassword.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {API_URL} from '../services';
+import {validateEmail} from '../constants/validate';
+
+export const useManagerSendMailResetPassword = () => {
+  const [email, setEmail] = React.useState('');
+  const [emailError, setEmailError] = React.useState('');
+  const [isProgressing, setProgressing] = React.useState(false);
+
+  const submit = async cb => {
+    setProgressing(true);
+    if (!validateEmail(email)) {
+      setEmailError('Email format is wrong!');
+      cb(null);
+      setProgressing(false);
+      return;
+    } else {
+      setEmailError('');
+    }
+    const response = await fetch(`${API_URL}/api/auth/forgot-password`, {
+      body: JSON.stringify({
+        email,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+    if (response.ok) {
+      let json = await response.json();
+      cb(json);
+    } else {
+      cb(null);
+    }
+    setProgressing(false);
+  };
+
+  return {
+    email,
+    setEmail,
+    isProgressing,
+    submit,
+    emailError,
+  };
+};
